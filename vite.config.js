49b0d00de3b922d770acbd06e1d48db3fc556e1b
Anshuman2341 <anshuman2341@gmail.com>
2025-06-14 12:23:08 +0530

    Configure GitHub Pages deployment
    bbb

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/create-react/', // 👈 Add this line with your repo name
   plugins: [react()],
 })
